c8d0c5b79c6d5c72dd0476973b28624ad3906a48
Michel <mikael.royet@gmail.com>
2023-08-23 15:21:38 +0200

    Character control (2)

--- CharacterController.gd
+++ CharacterController.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-
+var oldDirection = Vector2.ZERO
 const SPEED = 750.0
+
 
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
-
+	var direction = Input.get_vector("left", "right", "up", "down").normalized()
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		oldDirection = direction
+	else:
+		velocity = oldDirection * SPEED
 	move_and_slide()
