18de36307d22ac09ecb05890b3b97e4edd71cb9b
Michel <mikael.royet@gmail.com>
2023-10-13 01:09:21 +0200

    Mise en place des murs avec Path2D et Line2D

---
+++ Scenes/Wall.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var curve = $Path2D.curve
+	var polygon = curve.get_baked_points()
+	print($CollisionPoly.polygon)
+	$CollisionPoly.polygon = polygon
+	$Line2D.points = polygon
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
