b433d28c0a14fd0843af45547cc23e7178a89258
Michel <mikael.royet@gmail.com>
2023-11-14 17:37:28 +0100

    Test BG on level 1

--- scripts/Wall.gd
+++ scripts/Wall.gd
@@ -6,10 +6,12 @@ func _ready():
 	var curve = $Path2D.curve
 	var occultPoly = $LightOccluder2D
 	var polygon = curve.get_baked_points()
+	var polyRoad = $PolygonRoadBG
 	print(polygon.size())
 	$CollisionPoly.polygon = polygon
 	$Line2D.points = polygon
 	occultPoly.get_occluder_polygon().polygon = polygon
+	polyRoad.polygon = polygon
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
